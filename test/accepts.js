c4ae4a4fac5008e86cb75e63ec58e90f401f136d
Kevin Swiber <kswiber@gmail.com>
2012-01-06 22:52:06 -0500

    Piecing  together Makefile and test directory.

---
+++ test/accepts.js
@@ -0,0 +1,9 @@
+//var leisure = require('../');
+
+describe('leisure', function() {
+  describe('#accepts', function() {
+    it('should accept the first matching media type by highest weight', function() {
+      
+    });
+  });
+});
