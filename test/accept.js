3f48f8645620d7e3c122adde8f14924da6d4c0cc
Kevin Swiber <kevinswiber@quickenloans.com>
2012-01-08 10:09:36 -0500

    Removed underscore.js, added test for response content type.

--- test/accept.js
+++ test/accept.js
@@ -35,5 +35,32 @@ describe('leisure', function() {
 
       accept(req, res, next);
     });
+
+    it('should accept single matching media types with formats specified', function(done) {
+      var mediaTypes = [{ contentType: 'vnd.shop.Order', formats: ['json', 'xml'] }];
+      var accept = leisure.accept(mediaTypes);
+      req.headers['accept'] = 'vnd.shop.Order+json';
+
+      var next = function() {
+        req.accepted.contentType.should.equal('vnd.shop.Order');
+        req.accepted.format.should.equal('json');
+        done();
+      };
+
+      accept(req, res, next);
+    });
+
+    it('should set the response content-type to the appropriate media type', function(done) {
+      var mediaTypes = [{ contentType: 'text/xml' }];
+      var accept = leisure.accept(mediaTypes);
+      req.headers['accept'] = 'text/xml';
+
+      var next = function() {
+        res.headers['content-type'].should.equal('text/xml');
+        done();
+      };
+
+      accept(req, res, next);
+    });
   });
 });
